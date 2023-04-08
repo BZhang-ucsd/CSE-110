ab4b12470cb180fc8ea8b5c862f9bd927437eacc
BZhang-ucsd <97600878+BZhang-ucsd@users.noreply.github.com>
2023-04-08 09:38:50 -0700

    Update HelloWorld.c

--- HelloWorld.c
+++ HelloWorld.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 int main() {
-   // printf() displays the string inside quotation
    printf("Hello, World!");
    printf("My favorite programming language is C!");
    return 0;
